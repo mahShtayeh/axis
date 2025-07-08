4c966d51ebb3ec4ba90308e00564265f1bb6c9ab
Mahmoud Shtayeh <mshtayeh.c@stc.com.sa>
2025-07-08 10:39:11 +0300

    Configure web server

--- src/main/java/com/axis/account/AccountApplication.java
+++ src/main/java/com/axis/account/AccountApplication.java
@@ -5,9 +5,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Axis financial account main class
+ *
+ * @author Mahmoud Shtayeh
+ */
 @SpringBootApplication
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AccountApplication {
+    /**
+     * Application main entry point
+     *
+     * @param args Passed in command line arguments
+     */
     public static void main(final String[] args) {
         SpringApplication.run(AccountApplication.class, args);
     }
